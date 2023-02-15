f3ef9bdd63c746c4c1c4838297f14dcc855aae1f
Alexandre <alexandmag@gmail.com>
2023-02-15 10:18:43 -0300

    made the menu buttom dinamic

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,34 @@ $(function(){
     //O que vai acontecer quando clicar em nav.mobile
     $('nav.mobile').click(function(){
 
-        var listaMenu = $('nav.mobile ul');
-
-        /*
-        if(listaMenu.is(':hidden') == true)
-            listaMenu.fadeIn();
-        else
-            listaMenu.fadeOut();
-        */
-
-        listaMenu.slideToggle();
-
-        /*
-        if(listaMenu.is(':hidden') == true)
-            //listaMenu.show();
-            listaMenu.css('display','block');
-        else
-            //listaMenu.hide();
-            listaMenu.css('display','none');
-        */
-        })
+        var listaMenu = $('nav.mobile ul');  
+        var icone = $('.botao-menu-mobile i');
+        
+        if(listaMenu.is(':hidden') == true){
+            icone.removeClass('fa-bars');
+            icone.addClass('fa-times')
+            listaMenu.slideToggle();
+        }
+        else{
+            icone.removeClass('fa-times')
+            icone.addClass('fa-bars');
+            listaMenu.slideToggle(); 
+        }
+    })
 })
+
+/*
+        if(listaMenu.is(':hidden') == true)
+        listaMenu.fadeIn();
+        else
+        listaMenu.fadeOut();
+        */
+        
+        /*
+        if(listaMenu.is(':hidden') == true)
+        //listaMenu.show();
+        listaMenu.css('display','block');
+        else
+        //listaMenu.hide();
+        listaMenu.css('display','none');
+        */
